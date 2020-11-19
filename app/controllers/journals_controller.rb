ac09347cc7764d269541545c6ff6220edb4cba08
ramanujan <ramanujan@agent97.agent97>
2020-11-19 01:33:20 -0500

    Almost done with section 4

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -2,7 +2,43 @@ class JournalsController < ApplicationController
     def show
         @journal = Journal.find(params[:id])
     end
+
     def index
         @journals = Journal.all
     end
+
+    def create
+        @journal = Journal.new(params.require(:journal).permit(:title, :description))
+        if @journal.save
+            flash[:notice] = "Journal was saved in the database"
+            redirect_to @journal
+        else
+            render 'new'
+        end
+    end
+
+    def new
+        @journal = Journal.new
+    end
+
+    def edit
+        @journal = Journal.find(params[:id])
+    end
+
+    def update
+        @journal = Journal.find(params[:id])
+        if @journal.update(params.require(:journal).permit(:title, :description))
+            flash[:notice] = "Journal updated"
+            redirect_to @journal
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @journal = Journal.find(params[:id])
+        @journal.destroy
+        redirect_to journals_path
+    end
+
 end
